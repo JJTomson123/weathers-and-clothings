a865542fbc7be8177fd36d3bc42ad26b9f9f338e
KHsuan <50944459+KHsuan@users.noreply.github.com>
2021-01-12 10:37:30 +0800

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flaskblog import getw
+from flaskblog import getwweek
 
 getw.getw()
+getwweek.getw()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ddff290515c29a5e706c9029f54a9c52'
